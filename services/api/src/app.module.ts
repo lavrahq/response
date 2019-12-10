7bf4cb01d2cfdbbedad1ee67017207cd9b7ee3d0
Scott Plunkett <scottplunkett15@gmail.com>
2019-12-10 11:58:59 -0600

    chore(api): remove graphql module reference
    Signed-off-by: Scott Plunkett <scottplunkett15@gmail.com>

--- services/api/src/app.module.ts
+++ services/api/src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { GraphqlModule } from './graphql/graphql.module';
 import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [GraphqlModule, ConfigModule],
+  imports: [ConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
